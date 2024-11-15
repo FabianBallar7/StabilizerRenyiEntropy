cee8b20b7f53cfe157c91f9b338650ae63c7cd5a
Fabian Ballar <77710985+FabianBallar7@users.noreply.github.com>
2024-11-15 14:42:04 +0100

    Add files via upload
    Adding mixed state n=2 computation

--- QMagic-Parallel.jl
+++ QMagic-Parallel.jl
@@ -64,6 +64,16 @@ end
 end
 
 
+@everywhere function config_to_index(s)
+    N = length(s)
+    s_n = (s .+ 1)/2
+    
+    s_to_basis = Int(dot(s_n,(2 .^ (0:N-1)))) + 1
+    
+    return s_to_basis
+    
+end
+
 
 @everywhere function Psi_s(s,vec)
     N = length(s)
@@ -123,7 +133,47 @@ end
 
 
 
+#n=2 Renyi entropy for mixed states (subtracting log purity)
+function M2_mixed(rho,ncores)
+    
+    expectation_numerator = 0
+    expectation_denominator = 0
+    N = Int(log2(size(rho)[1]))
+    
+    out_per_core_num = SharedArray{ComplexF64}(ncores) 
+    out_per_core_den = SharedArray{ComplexF64}(ncores) 
+    
+    
+    bin_size = Int(ceil((4^N - 1)/ncores))
+    
+    worker_batches = collect(Iterators.partition(0:(4^N - 1), bin_size))
 
+    @sync @distributed for k = 1:ncores
+        for i in worker_batches[k]
+            P = digits(i,base=4,pad=N)
+            spin_sum = 0
+            for j=0:2^N - 1
+                s = 2 .* digits(j,base=2,pad=N) .- 1
+                alpha, Ps = Σs(P,s)
+                spin_sum += rho[j+1,config_to_index(Ps)] * alpha
+            end
+            expectation_numerator += spin_sum^4
+            expectation_denominator += spin_sum^2
+        end
+        out_per_core_num[k] = expectation_numerator
+        out_per_core_den[k] = expectation_denominator
+    end    
+
+    out = -1*log(sum(out_per_core_num)/sum(out_per_core_den))
+    
+    if abs(imag(out)) > 10^(-12) 
+        @warn "Warning imaginary part is non-zero."
+        return out
+    else
+        return real(out)
+    end
+    
+end
 
 
 
--- QMagic.jl
+++ QMagic.jl
@@ -54,6 +54,15 @@ function Σs(P,s)
     return (coef,s_)
 end
 
+function config_to_index(s)
+    N = length(s)
+    s_n = (s .+ 1)/2
+    
+    s_to_basis = Int(dot(s_n,(2 .^ (0:N-1)))) + 1
+    
+    return s_to_basis
+    
+end
 
 
 function Psi_s(s,vec)
@@ -99,7 +108,36 @@ function Mn(n,psi)
 end
 
 
+#n=2 Renyi entropy for mixed states (subtracting log purity)
+function M2_mixed(rho)
+    
+    expectation_numerator = 0
+    expectation_denominator = 0
+    N = Int(log2(size(rho)[1]))
 
+
+    @showprogress for i=0:4^N-1 
+        P = digits(i,base=4,pad=N)
+        spin_sum = 0
+        for j=0:2^N - 1
+            s = 2 .* digits(j,base=2,pad=N) .- 1
+            alpha, Ps = Σs(P,s)
+            spin_sum += rho[j+1,config_to_index(Ps)] * alpha 
+        end
+        expectation_numerator += spin_sum^4
+        expectation_denominator += spin_sum^2
+    end
+    
+    out = -1*log(expectation_numerator/expectation_denominator)
+    
+    if abs(imag(out)) > 10^(-12) 
+        @warn "Warning imaginary part is non-zero."
+        return out
+    else
+        return real(out)
+    end
+    
+end
 
 
 
